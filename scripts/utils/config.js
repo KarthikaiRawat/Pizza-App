5a65d653cc7a6d915e88e0b86874b8bee775e9f2
KarthikaiRawat <91263896+KarthikaiRawat@users.noreply.github.com>
2023-09-25 22:50:02 +0530

    Update config.js

--- scripts/utils/config.js
+++ scripts/utils/config.js
@@ -1,7 +1,7 @@
+ export const URL =
+   "https://raw.githubusercontent.com/brainmentorspvtltd/DIT_FullStack/main/pizza.json";
 // export const URL =
-//   "https://raw.githubusercontent.com/brainmentorspvtltd/DIT_FullStack/main/pizza.json";
-export const URL =
-  "https://raw.githubusercontent.com/Skill-risers/pizzajson/main/pizza.json";
+//   "https://raw.githubusercontent.com/Skill-risers/pizzajson/main/pizza.json";
 
 // export const URL =
 //   "https://raw.githubusercontent.com/elastic/Example-Watches/master/lateral_movement_in_user_comm/watch.json";
