711a2fd685162dd667132f0eb1710201f7862671
KarthikaiRawat <91263896+KarthikaiRawat@users.noreply.github.com>
2023-10-10 17:44:43 +0530

    Update config.js

--- scripts/utils/config.js
+++ scripts/utils/config.js
@@ -1,5 +1,5 @@
  export const URL =
-   "https://gist.githubusercontent.com/KarthikaiRawat/6d2e11161c5d137e20938ea9868d03dd/raw/f212b0c9e31fd5add9a71f095f67d49a91490d77/pizza.json";
+  "https://gist.githubusercontent.com/KarthikaiRawat/6d2e11161c5d137e20938ea9868d03dd/raw/f212b0c9e31fd5add9a71f095f67d49a91490d77/pizza.json";
 // export const URL =
 //   "https://raw.githubusercontent.com/Skill-risers/pizzajson/main/pizza.json";
 
